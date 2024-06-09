96d7d758f94de2b9fef3619616611039d2139469
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-06-09 21:25:06 +0530

    Update AddFriend.js

--- AddFriend.js
+++ AddFriend.js
@@ -1,1 +1,22 @@
+let ischng = document.querySelector("#strgr")
+let btn = document.querySelector("#btn")
+flag = 0;
+btn.addEventListener("click",function(){
+    if(flag==0){
+    ischng.innerHTML="Friend";
+    ischng.style.color ="green";
+    btn.innerHTML="Remove";
+    btn.style.color="white";
+    btn.style.backgroundColor="red"
+    flag = 1;
+    }
+    else{
+        ischng.innerHTML="Stranger";
+    ischng.style.color ="Red";
+    btn.innerHTML="Add Friend";
+    btn.style.color="white";
+    btn.style.backgroundColor="cadetblue"; 
+    flag = 0;
+    }
 
+})
